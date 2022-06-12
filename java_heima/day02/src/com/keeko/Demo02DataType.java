a3e1c9d6b6be70eff6e2521e29f89bfe3e7d02e1
keeko <787018661@qq.com>
2022-06-12 19:20:22 +0800

    [feat] java DataType Char

--- java_heima/day02/src/com/keeko/Demo02DataType.java
+++ java_heima/day02/src/com/keeko/Demo02DataType.java
@@ -6,24 +6,29 @@ package com.keeko;
 	2. 格式：范围小的类型 范围小的变量名 = (范围小的类型) 原本范围大的数据;
 
 注意事项：
-	1. 强制类型转换一般不推荐使用，因为有可能发生精度损失、数据溢出。
+	1. 强制类型转换一般不推荐使用，因为有可能发生数据溢出、精度损失。
 	2. byte/short/char这三种类型都可以发生数学运算，例如加法“+”.
 	3. byte/short/char这三种类型在运算的时候，都会被首先提升成为int类型，然后再计算。
 	4. boolean类型不能发生数据类型转换
 */
 public class Demo02DataType {
     public static void main(String[] args) {
-        // 左边是int类型，右边是long类型，不一样
+        // 左边int，右边long，类型不一样
+        // int num = 100L;
         // long --> int，不是从小到大
         // 不能发生自动类型转换！
+
+        // 使用强制类型转换
         // 格式：范围小的类型 范围小的变量名 = (范围小的类型) 原本范围大的数据;
         int num = (int) 100L;
         System.out.println(num);
 
+        // 数据溢出 ↓
         // long强制转换成为int类型
         int num2 = (int) 6000000000L;
         System.out.println(num2); // 1705032704
 
+        // 精度损失 ↓
         // double --> int，强制类型转换
         int num3 = (int) 3.99;
         System.out.println(num3); // 3，这并不是四舍五入，所有的小数位都会被舍弃掉
@@ -35,6 +40,7 @@ public class Demo02DataType {
 
         byte num4 = 40; // 注意！右侧的数值大小不能超过左侧的类型范围
         byte num5 = 50;
+        // byte result1 = num4 + num5; // 报错
         // byte + byte --> int + int --> int
         int result1 = num4 + num5;
         System.out.println(result1); // 90
