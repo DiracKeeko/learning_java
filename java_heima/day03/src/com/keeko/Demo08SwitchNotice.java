5dc8f92f3b2be5130e4eca4f879afe4eacd38017
Keeko <787018661@qq.com>
2022-06-13 20:18:07 +0800

    [feat] java loop (for, while, do...while)

--- java_heima/day03/src/com/keeko/Demo08SwitchNotice.java
+++ java_heima/day03/src/com/keeko/Demo08SwitchNotice.java
@@ -6,11 +6,12 @@ switch语句使用的注意事项：
 1. 多个case后面的数值不可以重复。
 
 2. switch后面小括号当中只能是下列数据类型：
-基本数据类型：byte/short/char/int
+基本数据类型：byte/short/int/char
 引用数据类型：String字符串、enum枚举
 
-3. switch语句格式可以很灵活：前后顺序可以颠倒，而且break语句还可以省略。
-“匹配哪一个case就从哪一个位置向下执行，直到遇到了break或者整体结束为止。”
+3. switch语句格式可以很灵活：前后顺序可以颠倒（如default可以放在case 3上面），
+    而且break语句还可以省略。
+    “匹配哪一个case就从哪一个位置向下执行，直到遇到了break或者整体结束为止。”
 */
 public class Demo08SwitchNotice {
     public static void main(String[] args) {
@@ -28,6 +29,6 @@ public class Demo08SwitchNotice {
             default:
                 System.out.println("他好，我也好。");
                 break;
-        } // switch
+        }
     }
 }
