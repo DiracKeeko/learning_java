9094731323dca8ff6909f72084b39a7bbb214eb4
Keeko <787018661@qq.com>
2022-07-08 16:38:14 +0800

    [feat] java 接口的定义和使用 抽象方法

---
+++ java_heima/day10/src/com/keeko/demo01/MyInterfaceAbstractImpl.java
@@ -0,0 +1,30 @@
+package com.keeko.demo01;
+
+/*
+*
+*   在implements 自动补全接口名称之后之后 alt + enter 生成 implement methods
+*   也可以 选中实现的接口(MyInterfaceAbstract) alt + enter 生成 implement methods
+*
+* */
+public class MyInterfaceAbstractImpl implements MyInterfaceAbstract {
+
+    @Override
+    public void methodAbs1() {
+        System.out.println("这是第一个方法！");
+    }
+
+    @Override
+    public void methodAbs2() {
+        System.out.println("这是第二个方法！");
+    }
+
+    @Override
+    public void methodAbs3() {
+        System.out.println("这是第三个方法！");
+    }
+
+    @Override
+    public void methodAbs4() {
+        System.out.println("这是第四个方法！");
+    }
+}
