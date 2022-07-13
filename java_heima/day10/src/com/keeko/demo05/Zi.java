9da1ce0022edd394acbfb914c4919c8c76604c0f
Keeko <787018661@qq.com>
2022-07-13 16:58:03 +0800

    [feat] java 多态中成员方法的访问规则

--- java_heima/day10/src/com/keeko/demo05/Zi.java
+++ java_heima/day10/src/com/keeko/demo05/Zi.java
@@ -13,5 +13,12 @@ public class Zi extends Fu {
         System.out.println(num);
     }
 
+    @Override
+    public void method() {
+        System.out.println("子类方法");
+    }
 
+    public void methodZi() {
+        System.out.println("子类特有方法");
+    }
 }
