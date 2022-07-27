300d1001e3e91411ecaf711ed7a796f691e201ee
keeko <787018661@qq.com>
2022-07-27 22:37:57 +0800

    [feat] java Object类的toString方法

---
+++ java_heima2/day01/src/com/keeko/demo01/Object/Person.java
@@ -0,0 +1,52 @@
+package com.keeko.demo01.Object;
+
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    /*
+        直接打印对象的地址值没有意义,需要重写Object类中的toString方法
+        打印对象的属性(name,age)
+     */
+    /*@Override
+    public String toString() {
+       // return "abc";
+       return "Person{name="+name+" ,age="+age+"}";
+    }*/
+
+    // ↓ alt + insert 选择toString() 再enter 自动生成
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
