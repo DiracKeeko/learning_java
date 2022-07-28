df7c7de6532dc34e18bddc703b39ca981a24cb6a
Keeko <787018661@qq.com>
2022-07-28 17:23:14 +0800

    [feat] java Object类的equal方法(默认比较object地址)

--- java_heima2/day01/src/com/keeko/demo01/Object/Person.java
+++ java_heima2/day01/src/com/keeko/demo01/Object/Person.java
@@ -33,6 +33,38 @@ public class Person {
                 '}';
     }
 
+    /*
+        Object类的equals方法,默认比较的是两个对象的地址值,没有意义
+        所以我们要重写equals方法,比较两个对象的属性(name,age)
+        问题:
+            隐含着一个多态
+            多态的弊端:无法使用子类特有的内容(属性和方法)
+            Object obj = p2 = new Person("古力娜扎",19);
+            解决:可以使用向下转型(强转)把obj类型转换为Person
+     */
+    @Override
+    public boolean equals(Object obj) {
+        //增加一个判断,传递的参数obj如果是this本身,直接返回true,提高程序的效率
+        if(obj==this){
+            return true;
+        }
+
+        //增加一个判断,传递的参数obj如果是null,直接返回false,提高程序的效率
+        if(obj==null){
+            return false;
+        }
+
+        //增加一个判断,防止类型转换一次ClassCastException
+        if(obj instanceof Person){
+            //使用向下转型,把obj转换为Person类型
+            Person p = (Person)obj;
+            //比较两个对象的属性,一个对象是this(p1),一个对象是p(obj->p2)
+            return this.name.equals(p.name) && this.age==p.age;
+        }
+        //不是Person类型直接返回false
+        return false;
+    }
+
 
     public String getName() {
         return name;
