dd0f5540c4ccfc54875d944d2ad3e858e3f91163
Keeko <787018661@qq.com>
2022-08-11 19:36:43 +0800

    [feat] java 定义和使用含有泛型的接口02

--- java_heima2/day02_Collection/src/com/keeko/demo03/Generic/Demo04GenericInterface.java
+++ java_heima2/day02_Collection/src/com/keeko/demo03/Generic/Demo04GenericInterface.java
@@ -3,7 +3,11 @@ package com.keeko.demo03.Generic;
 /*
     测试含有泛型的接口
 
+    1、
     泛型接口  ->  泛型接口的实现类(指定类型)  ->  使用实现类
+
+    2、
+    泛型接口  ->  泛型接口的实现类(不指定类型，保留泛型)  ->  使用实现类(指定类型)
  */
 
 public class Demo04GenericInterface {
@@ -12,5 +16,11 @@ public class Demo04GenericInterface {
         GenericInterfaceImpl1 gi1 = new GenericInterfaceImpl1();
         gi1.method("字符串");
 
+        //创建GenericInterfaceImpl2对象
+        GenericInterfaceImpl2<Integer> gi2 = new GenericInterfaceImpl2<>();
+        gi2.method(10);
+
+        GenericInterfaceImpl2<Double> gi3 = new GenericInterfaceImpl2<>();
+        gi3.method(8.8);
     }
 }
