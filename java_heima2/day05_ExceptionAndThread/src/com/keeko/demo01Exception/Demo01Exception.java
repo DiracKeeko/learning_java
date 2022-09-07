6fd41e235f69b2e636999b14e8a8c8cafe75f233
Keeko <787018661@qq.com>
2022-09-07 17:16:56 +0800

    [feat] java 异常处理01 throws

--- java_heima2/day05_ExceptionAndThread/src/com/keeko/demo01Exception/Demo01Exception.java
+++ java_heima2/day05_ExceptionAndThread/src/com/keeko/demo01Exception/Demo01Exception.java
@@ -1,24 +1,25 @@
 package com.keeko.demo01Exception;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /*
     java.lang.Throwable:类是 Java 语言中所有错误或异常的超类。
         Exception:编译期异常,进行编译(写代码)java程序出现的问题
             RuntimeException:运行期异常,java程序运行过程中出现的问题
             异常就相当于程序得了一个小毛病(感冒,发烧),把异常处理掉,程序可以继续执行(吃点药,继续革命工作)
         Error:错误
-            错误就相当于程序得了一个无法治愈的毛病(非典,艾滋).必须修改源代码,程序才能继续执行
+            错误就相当于程序得了一个无法治愈的毛病(癌症,艾滋).必须修改源代码,程序才能继续执行
  */
 public class Demo01Exception {
-    public static void main(String[] args) /*throws ParseException*/ {
+    // 第1种处理异常的方式  通过 throws 交给虚拟机来处理。 ↓
+    // 缺点：一旦出问题，抛出异常，中断程序，打印错误信息到控制台
+    public static void main(String[] args) throws ParseException {
         //Exception:编译期异常,进行编译(写代码)java程序出现的问题
-        /*SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");//用来格式化日期
-        Date date = null;
-        try {
-            date = sdf.parse("1999-0909");//把字符串格式的日期,解析为Date格式的日期
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        System.out.println(date);*/
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");//用来格式化日期
+        Date date = sdf.parse("1999-0909");
+        System.out.println(date);
 
         //RuntimeException:运行期异常,java程序运行过程中出现的问题
         /*int[] arr = {1,2,3};
