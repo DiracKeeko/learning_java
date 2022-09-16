11380a67a9568ee4866b65a499e8cdb4f0dac893
keeko <787018661@qq.com>
2022-09-17 04:28:36 +0800

    [chore] 独立learning_java仓库

--- React/src/index07.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-const title = React.createElement("h1", null, "hello react");
-
-ReactDOM.render(title, document.getElementById("root"));
