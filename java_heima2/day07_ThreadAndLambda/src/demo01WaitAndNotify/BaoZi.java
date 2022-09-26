47a5f917f4c603486d87fd64184e7941c7f581d5
Keeko <787018661@qq.com>
2022-09-26 19:35:37 +0800

    [feat] 实现生产者BaoZi

---
+++ java_heima2/day07_ThreadAndLambda/src/demo01WaitAndNotify/BaoZi.java
@@ -0,0 +1,18 @@
+package demo01WaitAndNotify;
+
+/*
+* 资源类： 包子类
+* 设置包子的属性
+*   皮
+*   馅儿
+*   包子的状态  true  /  false
+* */
+
+public class BaoZi {
+    // 皮
+    String pi;
+    // 馅儿
+    String xian;
+    // 包子的状态, 初始值为false
+    boolean flag = false;
+}
