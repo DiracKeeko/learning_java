ec9420175c1da9ea792e60d5c435ec3db26127fb
keeko <787018661@qq.com>
2022-10-07 18:59:08 +0800

    [chore] java中的相对路径相对项目根目录

---
+++ java_heima2/day08_FileAndRecursion/src/com/keeko/demo01File/Demo02File.java
@@ -0,0 +1,27 @@
+package com.keeko.demo01File;
+
+
+import java.io.File;
+
+/*
+    路径:
+        绝对路径:是一个完整的路径
+            以盘符(c:,D:)开始的路径
+                c:\\a.txt
+                D:\\demo\\b.txt
+
+        相对路径:是一个简化的路径
+            相对指的是相对于当前项目的根目录(D:\\git\\learning_java\\java_heima2)
+            如果使用当前项目的根目录,路径可以简化书写
+            D:\\git\\learning_java\\java_heima2\\123.txt-->简化为: 123.txt
+            (可以省略项目的根目录)
+
+        注意:
+            1.路径是不区分大小写
+            2.路径中的文件名称分隔符windows使用反斜杠,反斜杠是转义字符,两个反斜杠代表一个普通的反斜杠
+ */
+
+public class Demo02File {
+
+}
+
