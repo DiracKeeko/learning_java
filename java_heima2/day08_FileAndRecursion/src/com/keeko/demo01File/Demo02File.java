090d5d107203a7b6717d8d6ad893081917144575
keeko <787018661@qq.com>
2022-10-07 19:16:08 +0800

    [feat] java File类的构造方法

--- java_heima2/day08_FileAndRecursion/src/com/keeko/demo01File/Demo02File.java
+++ java_heima2/day08_FileAndRecursion/src/com/keeko/demo01File/Demo02File.java
@@ -22,6 +22,67 @@ import java.io.File;
  */
 
 public class Demo02File {
+    public static void main(String[] args) {
+        /*
+            File类的构造方法
+         */
+        // show01();
+        // show02("c:\\","a.txt"); // c:\a.txt
+        // show02("d:\\","a.txt"); // d:\a.txt
+        show03();
+
+        File f = new File("C:\\Users\\itcast\\IdeaProjects\\shungyuan");
+        long length = f.length();
+        System.out.println(length);
+    }
+
+    /*
+        File(String pathname) 通过将给定路径名字符串转换为抽象路径名来创建一个新 File 实例。
+        参数:
+            String pathname:字符串的路径名称
+            路径可以是以文件结尾,也可以是以文件夹结尾
+            路径可以是相对路径,也可以是绝对路径
+            路径可以是存在,也可以是不存在
+            创建File对象,只是把字符串路径封装为File对象,不考虑路径的真假情况
+     */
+    private static void show01() {
+        File f1 = new File("D:\\git\\learning_java\\java_heima2\\a.txt"); // 不存在，是个假路径
+        System.out.println(f1); // D:\git\learning_java\java_heima2\a.txt
+        //重写了Object类的toString方法
+
+        File f2 = new File("D:\\git\\learning_java\\java_heima2");
+        System.out.println(f2); // D:\git\learning_java\java_heima2
+
+        File f3 = new File("b.txt");
+        System.out.println(f3); // b.txt
+    }
+
+    /*
+        File(String parent, String child) 根据 parent 路径名字符串和 child 路径名字符串创建一个新 File 实例。
+        参数:把路径分成了两部分
+            String parent:父路径
+            String child:子路径
+        好处:
+            父路径和子路径,可以单独书写,使用起来非常灵活;父路径和子路径都可以变化
+     */
+    private static void show02(String parent, String child) {
+        File file = new File(parent, child);
+        System.out.println(file); // c:\a.txt
+    }
+
+    /*
+        File(File parent, String child) 根据 parent 抽象路径名和 child 路径名字符串创建一个新 File 实例。
+        参数:把路径分成了两部分
+            File parent:父路径
+            String child:子路径
+        好处:
+             父路径和子路径,可以单独书写,使用起来非常灵活;父路径和子路径都可以变化
+             父路径是File类型,可以使用File的方法对路径进行一些操作,再使用路径创建对象
+     */
+    private static void show03() {
+        File parent = new File("c:\\");
+        File file = new File(parent, "hello.java");
+        System.out.println(file); // c:\hello.java
+    }
 
 }
-
