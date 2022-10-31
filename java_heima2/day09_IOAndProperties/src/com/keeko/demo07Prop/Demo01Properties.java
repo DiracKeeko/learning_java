8e774cca00edccdfb7c73214cf664eaef811bc68
Keeko <787018661@qq.com>
2022-10-31 19:59:31 +0800

    [feat] Properties集合存储和读取

---
+++ java_heima2/day09_IOAndProperties/src/com/keeko/demo07Prop/Demo01Properties.java
@@ -0,0 +1,49 @@
+package com.keeko.demo07Prop;
+
+import java.io.IOException;
+import java.util.Properties;
+import java.util.Set;
+
+/*
+    java.util.Properties集合 extends Hashtable<k,v> implements Map<k,v>
+    Properties 类表示了一个持久的属性集。Properties 可保存在流中或从流中加载。
+    Properties集合是一个唯一和IO流相结合的集合
+        可以使用Properties集合中的方法store,把集合中的临时数据,持久化写入到硬盘中存储
+        可以使用Properties集合中的方法load,把硬盘中保存的文件(键值对),读取到集合中使用
+
+    属性列表中每个键及其对应值都是一个字符串。
+        Properties集合是一个双列集合,key和value默认都是字符串
+ */
+public class Demo01Properties {
+    public static void main(String[] args) throws IOException {
+        show01();
+    }
+
+    /*
+        使用Properties集合存储数据,遍历取出Properties集合中的数据
+        Properties集合是一个双列集合,key和value默认都是字符串
+        Properties集合有一些操作字符串的特有方法
+            Object setProperty(String key, String value) 调用 Hashtable 的方法 put。
+            String getProperty(String key) 通过key找到value值,此方法相当于Map集合中的get(key)方法
+            Set<String> stringPropertyNames() 返回此属性列表中的键集，其中该键及其对应值是字符串,此方法相当于Map集合中的keySet方法
+     */
+    private static void show01() {
+        //创建Properties集合对象
+        Properties prop = new Properties();
+        //使用setProperty往集合中添加数据
+        prop.setProperty("赵丽颖", "168");
+        prop.setProperty("迪丽热巴", "165");
+        prop.setProperty("古力娜扎", "160");
+        // prop.put(1, true); // put放入的类型可以是任意类型，所以不要用put
+
+        //使用stringPropertyNames把Properties集合中的键取出,存储到一个Set集合中
+        Set<String> set = prop.stringPropertyNames();
+
+        //遍历Set集合,取出Properties集合的每一个键
+        for (String key : set) {
+            //使用getProperty方法通过key获取value
+            String value = prop.getProperty(key);
+            System.out.println(key + "=" + value);
+        }
+    }
+}
