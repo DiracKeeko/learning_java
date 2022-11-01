d8dc692ed32430be0bc7a17c06c263b8baeab4fb
Keeko <787018661@qq.com>
2022-11-01 16:47:32 +0800

    [feat] Properties集合中的store方法

--- java_heima2/day09_IOAndProperties/src/com/keeko/demo07Prop/Demo01Properties.java
+++ java_heima2/day09_IOAndProperties/src/com/keeko/demo07Prop/Demo01Properties.java
@@ -1,5 +1,7 @@
 package com.keeko.demo07Prop;
 
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.Set;
@@ -16,7 +18,8 @@ import java.util.Set;
  */
 public class Demo01Properties {
     public static void main(String[] args) throws IOException {
-        show01();
+        // show01();
+        show02();
     }
 
     /*
@@ -45,5 +48,45 @@ public class Demo01Properties {
             String value = prop.getProperty(key);
             System.out.println(key + "=" + value);
         }
+    }
+
+
+    /*
+        可以使用Properties集合中的方法store,把集合中的临时数据,持久化写入到硬盘中存储
+        void store(OutputStream out, String comments)
+        void store(Writer writer, String comments)
+        参数:
+            OutputStream out:字节输出流,不能写入中文
+            Writer writer:字符输出流,可以写中文
+            String comments:注释,用来解释说明保存的文件是做什么用的
+                    不能使用中文,会产生乱码,默认是Unicode编码
+                    一般使用""空字符串
+
+        使用步骤:
+            1.创建Properties集合对象,添加数据
+            2.创建 字节输出流/字符输出流 对象,构造方法中绑定要输出的目的地
+            3.使用Properties集合中的方法store,把集合中的临时数据,持久化写入到硬盘中存储
+            4.释放资源
+     */
+    private static void show02() throws IOException {
+        //1.创建Properties集合对象,添加数据
+        Properties prop = new Properties();
+        prop.setProperty("赵丽颖", "168");
+        prop.setProperty("迪丽热巴", "165");
+        prop.setProperty("古力娜扎", "160");
+
+        /*
+        //2.创建字节输出流/字符输出流对象,构造方法中绑定要输出的目的地
+        FileWriter fw = new FileWriter("day09_IOAndProperties\\prop.txt");
+
+        //3.使用Properties集合中的方法store,把集合中的临时数据,持久化写入到硬盘中存储
+        prop.store(fw,"save data");
+
+        //4.释放资源
+        fw.close();
+        */
+
+        prop.store(new FileOutputStream("day09_IOAndProperties\\prop2.txt"), "");
+    //     匿名对象使用完自动关闭流对象
     }
 }
