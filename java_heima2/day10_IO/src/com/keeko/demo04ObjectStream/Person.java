e055fcd31fca281995f911ec5a4fdfcb4b390d88
Keeko <787018661@qq.com>
2022-11-10 17:06:44 +0800

    [chore] ObjectOutputStream序列化并存储对象

---
+++ java_heima2/day10_IO/src/com/keeko/demo04ObjectStream/Person.java
@@ -0,0 +1,53 @@
+package com.keeko.demo04ObjectStream;
+
+import java.io.Serializable;
+
+/*
+    序列化和反序列化的时候,会抛出NotSerializableException没有序列化异常
+    类通过实现 java.io.Serializable 接口以启用其序列化功能。未实现此接口的类将无法使其任何状态序列化或反序列化。
+
+    Serializable接口也叫标记型接口
+        要进行序列化和反序列化的类必须实现Serializable接口,就会给类添加一个标记
+        当我们进行序列化和反序列化的时候,就会检测类上是否有这个标记
+            有:就可以序列化和反序列化
+            没有:就会抛出 NotSerializableException异常
+    去市场买肉-->肉上有一个蓝色章(检测合格)-->放心购买-->买回来怎么吃随意
+
+ */
+public class Person implements Serializable{
+    private String name;
+    public int age;
+
+    public Person() {
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
