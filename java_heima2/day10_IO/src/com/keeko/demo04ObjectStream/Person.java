6308206d94290bb9b949e49862693b8cff754ea0
Keeko <787018661@qq.com>
2022-11-14 17:06:24 +0800

    [feat] 瞬态关键字 transient02

--- java_heima2/day10_IO/src/com/keeko/demo04ObjectStream/Person.java
+++ java_heima2/day10_IO/src/com/keeko/demo04ObjectStream/Person.java
@@ -31,7 +31,8 @@ import java.io.Serializable;
  */
 public class Person implements Serializable{
     private String name;
-    public int age;
+    // private static int age;
+    private transient int age;
 
     public Person() {
     }
