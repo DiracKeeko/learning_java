f78ccda92f21e04a78a02c82135ca3008269db03
Keeko <787018661@qq.com>
2023-01-07 17:29:40 +0800

    [feat] 反射reflect class对象功能 获取Constructor

---
+++ java_heima2/day14_baseEnhance/src/com/keeko/demo02/reflect/ReflectDemo3.java
@@ -0,0 +1,80 @@
+package com.keeko.demo02.reflect;
+
+
+import com.keeko.demo02.domain.Person;
+
+import java.lang.reflect.Constructor;
+
+public class ReflectDemo3 {
+
+    /**
+     Class对象功能：
+     * 获取功能：
+     1. 获取成员变量们
+     * Field[] getFields()
+     * Field getField(String name)
+
+     * Field[] getDeclaredFields()
+     * Field getDeclaredField(String name)
+     2. 获取构造方法们
+     * Constructor<?>[] getConstructors()
+     * Constructor<T> getConstructor(类<?>... parameterTypes)
+
+     * Constructor<T> getDeclaredConstructor(类<?>... parameterTypes)
+     * Constructor<?>[] getDeclaredConstructors()
+     3. 获取成员方法们：
+     * Method[] getMethods()
+     * Method getMethod(String name, 类<?>... parameterTypes)
+
+     * Method[] getDeclaredMethods()
+     * Method getDeclaredMethod(String name, 类<?>... parameterTypes)
+
+     4. 获取类名
+     * String getName()
+
+
+
+     */
+
+    public static void main(String[] args) throws Exception {
+
+        //0.获取Person的Class对象
+        Class personClass = Person.class;
+        /*
+            2. 获取构造方法们
+                 * Constructor<?>[] getConstructors()
+                 * Constructor<T> getConstructor(类<?>... parameterTypes)
+
+                 * Constructor<T> getDeclaredConstructor(类<?>... parameterTypes)
+                 * Constructor<?>[] getDeclaredConstructors()
+         */
+
+
+        //Constructor<T> getConstructor(类<?>... parameterTypes)
+        Constructor constructor = personClass.getConstructor(String.class, int.class);
+        System.out.println(constructor);
+        //创建对象
+        Object person = constructor.newInstance("张三", 23);
+        System.out.println("person");
+        System.out.println(person);
+
+        System.out.println("----------");
+
+
+        Constructor constructor1 = personClass.getConstructor();
+        System.out.println(constructor1);
+        //创建对象
+        Object person1 = constructor1.newInstance();
+        System.out.println("person1 空参");
+        System.out.println(person1);
+
+        // 空参的构造可以用personClass.newInstance()直接实现，省略创建构造器的步骤
+        Object o = personClass.newInstance();
+        System.out.println(o);
+
+
+        //constructor1.setAccessible(true);
+    }
+
+
+}
