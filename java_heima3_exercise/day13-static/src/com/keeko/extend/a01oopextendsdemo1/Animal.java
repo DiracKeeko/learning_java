b2a34b3281d7f12f2f79d199db073276bb47e8a2
Keeko <787018661@qq.com>
2023-05-18 17:23:39 +0800

    [chore] 继承 extends 的简单使用

---
+++ java_heima3_exercise/day13-static/src/com/keeko/extend/a01oopextendsdemo1/Animal.java
@@ -0,0 +1,17 @@
+package com.keeko.extend.a01oopextendsdemo1;
+
+public class Animal {
+    //权限修饰符：
+    //private：子类就无法访问了
+    //私有：只能在本类中访问
+
+    //注意事项:
+    //子类只能访问父类中非私有的成员
+    public void eat(){
+        System.out.println("吃东西");
+    }
+
+    public void drink(){
+        System.out.println("喝水");
+    }
+}
---
+++ java_heima3_exercise/day13-static/src/com/keeko/extend/a01oopextendsdemo1/Ragdoll.java
@@ -0,0 +1,4 @@
+package com.keeko.extend.a01oopextendsdemo1;
+
+public class Ragdoll extends Cat {
+}
