4362df4874ac8699eff03b059b282a2ba733be88
Keeko <787018661@qq.com>
2023-05-28 21:57:10 +0800

    [feat] 继承&重写父类方法 @override 02

---
+++ java_heima3_exercise/day13-static/src/com/keeko/extend/a07oopextendsdemo7/Husky.java
@@ -0,0 +1,11 @@
+package com.keeko.extend.a07oopextendsdemo7;
+
+public class Husky extends Dog {
+
+    //哈士奇有一个额外的方法拆家
+    public void breakHome() {
+        System.out.println("哈士奇又在拆家了");
+    }
+
+}
+
