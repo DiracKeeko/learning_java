f593d51ed0b9e0776f7c6968826567fbf119370f
Keeko <787018661@qq.com>
2023-05-29 16:53:48 +0800

    [feat] 继承 调用父类构造方法super()

---
+++ java_heima3_exercise/day13-static/src/com/keeko/extend/a08oopextendsdemo8/Test.java
@@ -0,0 +1,11 @@
+package com.keeko.extend.a08oopextendsdemo8;
+
+public class Test {
+    public static void main(String[] args) {
+        Student s = new Student();
+
+        //创建学生对象
+        Student s1 = new Student("zhangsan", 23);
+
+    }
+}
