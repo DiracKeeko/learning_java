ca74b3de973e96d96f88f3005831adc594127ae7
Keeko <787018661@qq.com>
2024-01-02 14:40:00 +0800

    [feat] 类型转换，可能导致空指针的情况处理

--- java_learning_from_work/src/com/keeko/BigDecimalProblem.java
+++ java_learning_from_work/src/com/keeko/BigDecimalProblem.java
@@ -16,7 +16,13 @@ public class BigDecimalProblem {
         fundListOrigin.add(new FundItemDo("2", "易方达蓝筹", new BigDecimal(2)));
         fundListOrigin.add(new FundItemDo("3", "中银稳健添利E", new BigDecimal(3)));
 
+        // main里面的代码看起来没有问题
+        // 但是一旦加入下面这行，就会因为 item.getM1Return()为null, 导致null.doubleValue报错： java.lang.NullPointerException
+        fundListOrigin.add(new FundItemDo("4", "广发北证50成分C", null));
+
+        // 正确的处理方式是 先过滤，将所有的可能为空又会有转换的情况 (可能导致报错的项过滤掉)
         List<FundResDto> fundListRes = fundListOrigin.stream()
+                .filter(item -> item.getM1Return() != null)
                 .map(item -> new FundResDto(item.getId(), item.getName(), item.getM1Return().doubleValue()))
                 .collect(Collectors.toList());
 
