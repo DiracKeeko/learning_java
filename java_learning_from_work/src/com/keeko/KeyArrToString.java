b735063159a17c5ea966c2f7589c9d37979f0e0b
Keeko <787018661@qq.com>
2024-05-17 15:03:00 +0800

    [feat] 获取value Arr，并转化为String

--- java_learning_from_work/src/com/keeko/KeyArrToString.java
+++ java_learning_from_work/src/com/keeko/KeyArrToString.java
@@ -1,11 +1,20 @@
 package com.keeko;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.StringJoiner;
 
-public class CreateObjectAndGetKeyArr {
+public class KeyArrToString {
+    // 使用 Java 9+ 的 Map.of() 方法来一次性创建并初始化 Map
+    /*private static final Map<String, String> indexObj = Map.of(
+            "000001.SS", "上证指数",
+            "000300.SS", "沪深300",
+            "000905.SS", "中证500",
+            "399001.SZ", "深证成指",
+            "399006.SZ", "创业板指"
+    );*/
+
     private static Map<String, String> createIndexMap() {
         Map<String, String> map = new HashMap<>();
         map.put("000001.SS", "上证指数");
@@ -20,8 +29,8 @@ public class CreateObjectAndGetKeyArr {
     private static final Map<String, String> indexMap = createIndexMap();
 
     public static void main(String[] args) {
-        Set<String> keySet = indexMap.keySet();
-        // System.out.println("keySet->" + keySet); // keySet->[000001.SS, 000300.SS, 000905.SS, 399001.SZ, 399006.SZ]
+        Collection<String> keySet = indexMap.values();
+        // System.out.println("keySet->" + keySet); // keySet->[上证指数, 沪深300, 中证500, 深证成指, 创业板指]
 
         // 创建一个 StringJoiner 用于拼接字符串
         StringJoiner joiner = new StringJoiner(",");
@@ -36,6 +45,6 @@ public class CreateObjectAndGetKeyArr {
 
         // 输出结果
         System.out.println("indexString-> " + indexString);
-        // indexString-> 000001.SS,000300.SS,000905.SS,399001.SZ,399006.SZ
+        // indexString-> 上证指数,沪深300,中证500,深证成指,创业板指
     }
 }
