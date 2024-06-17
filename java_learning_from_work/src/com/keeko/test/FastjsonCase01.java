caa637102a9de173fd6c78f823cbf4f3eed83481
Keeko <787018661@qq.com>
2024-06-17 16:46:12 +0800

    [chore] alibaba fastjson1.2.83 使用

---
+++ java_learning_from_work/src/com/keeko/test/FastjsonCase01.java
@@ -0,0 +1,17 @@
+package com.keeko.test;
+
+
+import com.alibaba.fastjson.JSON;
+import com.keeko.entity.User;
+
+// 1. 将 Java 对象序列化为 JSON 字符串
+public class FastjsonCase01 {
+    public static void main(String[] args) {
+        User user = new User();
+        user.setId(1);
+        user.setName("John Doe");
+
+        String jsonString = JSON.toJSONString(user);
+        System.out.println(jsonString); // 输出: {"id":1,"name":"John Doe"}
+    }
+}
---
+++ java_learning_from_work/src/com/keeko/test/FastjsonCase02.java
@@ -0,0 +1,15 @@
+package com.keeko.test;
+
+
+import com.alibaba.fastjson.JSON;
+import com.keeko.entity.User;
+
+// 2. 将 JSON 字符串反序列化为 Java 对象
+public class FastjsonCase02 {
+    public static void main(String[] args) {
+        String jsonString = "{\"id\":1,\"name\":\"John Doe\"}";
+
+        User user = JSON.parseObject(jsonString, User.class);
+        System.out.println(user.getName()); // 输出: John Doe
+    }
+}
