724b1d89aae9a922cc9a88e5eb92f9df1cfce933
Keeko <787018661@qq.com>
2024-11-29 15:39:17 +0800

    [feat] @RequestBody注解使用

--- java_learning_from_work/src/com/keeko/springMvc/controller/TestPostController.java
+++ java_learning_from_work/src/com/keeko/springMvc/controller/TestPostController.java
@@ -1,6 +1,7 @@
 package com.keeko.springMvc.controller;
 
 
+import com.keeko.entity.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -27,10 +28,57 @@ public class TestPostController {
 
             @PostMapping(value = "/list")
 
-                @PostMapping("/list") // 与 @PostMapping(value = "/list") 完全登记
+                @PostMapping("/list") // 与 @PostMapping(value = "/list") 完全等价
 
         总结：
             优先使用@PostMapping
     */
 
+    // case1
+    // @PostMapping + @RequestBody 接收参数
+    @PostMapping("/add")
+    public void add(@RequestBody User userRequest) {
+        System.out.println("Received User Info:");
+        System.out.println("name" + userRequest.getName());
+        System.out.println("id" + userRequest.getId());
+    }
+
+    /*
+    // case2
+    // @PostMapping + @RequestParam 接收参数  (不建议使用)
+    // 使用 @PostMapping 接收表单参数
+    @PostMapping("/addForm")
+    public ResponseEntity<String> addUserFromForm(@RequestParam String name,
+                                                  @RequestParam String email,
+                                                  @RequestParam int age) {
+        System.out.println("Received User Info from Form:");
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+        System.out.println("Age: " + age);
+        return ResponseEntity.ok("User added successfully via form");
+    }
+     */
+
+
+    /*
+    // case3
+    // 使用 @PostMapping 接收路径参数  (与 @RequestBody 搭配使用)
+    @PostMapping("/update/{id}")
+    public ResponseEntity<String> updateUser(@PathVariable Long id, @RequestBody User userRequest) {
+        System.out.println("Updating User ID: " + id);
+        System.out.println("New Name: " + userRequest.getName());
+        return ResponseEntity.ok("User updated successfully");
+    }
+     */
+
+    /*
+        其他说明：
+            1. @PostMapping 也可以像 @GetMapping 一样用 @RequestParam 接收url中query类型的参数 但这没有意义
+                (都用query了，直接用GET请求啊)
+
+            2. @PostMapping 使用@PathVariable接收 url中的路径参数，同时使用@RequestBody接收reqBody中的参数
+                (会用到，比较少，大部分的情况只用 @RequestBody 就可以了 路径中的参数信息也可以放到 reqBody中)
+
+            3. @RequestBody 也可以和 @RequestParam 混用，极其不推荐。
+     */
 }
